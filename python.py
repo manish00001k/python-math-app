8387428d52f9fc46014d6bf28d30c590bb84ceab
Manish Kumar <manish00001k@gmail.com>
2025-04-30 16:03:12 +0530

    Initial commit - Python calculator project1

---
+++ python.py
@@ -0,0 +1,12 @@
+# python.py
+
+import calculator
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("Add:", calculator.add(a, b))
+    print("Subtract:", calculator.subtract(a, b))
+    print("Multiply:", calculator.multiply(a, b))
+    print("Divide:", calculator.divide(a, b))
